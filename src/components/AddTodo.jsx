9a24d5b05099d378cefaebfd05fab95a293e035f
Shubhendu-12 <rajanmaurya100@gmail.com>
2024-01-01 23:30:54 +0530

    Redux Toolkit App Complete

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -24,7 +24,7 @@ const AddTodo = () => {
           <div className="">
             <label
               htmlFor="name"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white items-center justify-center "
+              className=" flex  mb-2 text-sm font-medium text-gray-900 dark:text-white items-center justify-center "
             >
               Notes
             </label>
@@ -37,13 +37,15 @@ const AddTodo = () => {
               placeholder="Notes"
             />
           </div>
+          <div className="flex items-center justify-center">
 
           <button
-            type="submiy"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            type="submit"
+            className="  text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
           >
             Add Todo
           </button>
+          </div>
         </form>
       </div>
     </>
